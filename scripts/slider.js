43a32f388df9e2350ff95966cef2adbe93a00cab
wizarddos <wizardkol123@gmail.com>
2021-07-09 21:12:15 +0200

    Feature - poprawiony slider.js i 2 dodatkowe sekcje

--- scripts/slider.js
+++ scripts/slider.js
@@ -31,33 +31,28 @@ function changePhoto(shouldGoBack){
 
     switch (updated) {
         case 1:
-            slider.innerHTML = '<img src="assets/ubuntu-desktop.jpg" class = "img" alt = "pulpit dystrybucji ubuntu"/><h3>Ubuntu - Niezawodny Start z Linuxem</h3><p>ubuntu - najpopularniejsze distro z przyjaznym środowiskiem GNOME<br/>Polecane na rozpoczęcie przygody z linuxem</p><br/><a href = "#" class = "distro-button" id = "distro-button">Pobierz Ubuntu</a>'
+            slider.innerHTML = '<img src="assets/ubuntu-desktop.jpg" class = "img" alt = "pulpit dystrybucji ubuntu"/><h3>Ubuntu - Niezawodny Start z Linuxem</h3><p>ubuntu - najpopularniejsze distro z przyjaznym środowiskiem GNOME<br/>Polecane na rozpoczęcie przygody z linuxem</p><br/><a href = "https://ubuntu.com/download/desktop" class = "distro-button" id = "distro-button">Pobierz Ubuntu</a>'
             $(sliderBackground).css( "background-color", "rgb(138, 48, 6)");
-            $(sliderDownload).css( "background-color", "brown");
             break;
     
         case 2:
-            slider.innerHTML = '<img src="assets/mint-desktop.png" class = "img" alt = "pulpit dystrybucji Linux Mint"/><h3>Linux Mint - Lekki cynamonowy OS</h3><p>Mint - to oparte na Ubuntu distro <br/> z własnym środowiskiem graficznym cinamon<br/>Również polecane na rozpoczęcie przygody z linuxem</p><br/><a href = "#" class = "distro-button" id = "distro-button">Pobierz Minta</a>'
-            $(sliderBackground).css( "background-color", "rgb(85, 245, 37)");
-            $(sliderDownload).css( "background-color", "lime");
+            slider.innerHTML = '<img src="assets/mint-desktop.png" class = "img" alt = "pulpit dystrybucji Linux Mint"/><h3>Linux Mint - Lekki cynamonowy OS</h3><p>Mint - to oparte na Ubuntu distro <br/> z własnym środowiskiem graficznym cinamon<br/>Również polecane na rozpoczęcie przygody z linuxem</p><br/><a href = "https://linuxmint.com/download.php" class = "distro-button" id = "distro-button">Pobierz Minta</a>'
+            $(sliderBackground).css( "background-color", "rgb(60, 200, 30)");
         break;
 
         case 3:
-            slider.innerHTML = '<img src="assets/manjaro-desktop.png" class = "img" alt = "pulpit dystrybucji Linux Manjaro"/><h3>Linux Manjaro - Dla wprawnych</h3><p>Manjarno - To oparte na Archu distro dla bardziej wprawnych użytkowników <br/>Polecane po zapoznaniu się z podstawami linuxa</p><br/><a href = "#" class = "distro-button" id = "distro-button">Pobierz Manjarno</a>'
+            slider.innerHTML = '<img src="assets/manjaro-desktop.png" class = "img" alt = "pulpit dystrybucji Linux Manjaro"/><h3>Linux Manjaro - Dla wprawnych</h3><p>Manjarno - To oparte na Archu distro dla bardziej wprawnych użytkowników <br/>Polecane po zapoznaniu się z podstawami linuxa</p><br/><a href = "https://manjaro.org/download/" class = "distro-button" id = "distro-button">Pobierz Manjarno</a>'
             $(sliderBackground).css( "background-color", "rgb(15, 165, 90)");
-            $(sliderDownload).css( "background-color", "rgb(13, 141, 77)");
         break;
 
         case 4:
-            slider.innerHTML = '<img src="assets/redhat-desktop.png" class = "img" alt = "pulpit dystrybucji Linux RedHat"/><h3>Linux RedHat - Płatny, dla wprawnych</h3><p>Redhat - Jedna z płatnych, choć z otwartym kodem źródłowym i najstarszych dystrybucji linuxa.<br/> Polecana dla zaawansowanych użytkoników i administratorów </p><br/><a href = "#" class = "distro-button" id = "distro-button">Kup RedHat Enterprise</a>'
+            slider.innerHTML = '<img src="assets/redhat-desktop.png" class = "img" alt = "pulpit dystrybucji Linux RedHat"/><h3>Linux RedHat - Płatny, dla wprawnych</h3><p>Redhat - Jedna z płatnych, choć z otwartym kodem źródłowym i najstarszych dystrybucji linuxa.<br/> Polecana dla zaawansowanych użytkoników i administratorów </p><br/><a href = "https://www.redhat.com/en/store/linux-platforms" class = "distro-button" id = "distro-button">Kup RedHat Enterprise</a>'
             $(sliderBackground).css( "background-color", "rgb(212, 20, 20)");
-            $(sliderDownload).css( "background-color", "red");  
         break;
 
         case 5:
-            slider.innerHTML = '<img src="assets/kali-desktop.jpg" class = "img" alt = "pulpit dystrybucji Linux Kali"/><h3>Linux kali - do rzeczy mniej lub barziej legalnych</h3><p>Kali Linux - dystrybucja stworzona do Hackingu i Pentestingu<br/> Zawiera preinstalowane narzędzia do hakowania i pentestingu <br/> Zdecydowanie odradzany dla nowych użytkowników</p><br/><a href = "#" class = "distro-button" id = "distro-button">Pobierz Kaliego</a>'
+            slider.innerHTML = '<img src="assets/kali-desktop.jpg" class = "img" alt = "pulpit dystrybucji Linux Kali"/><h3>Linux kali - do rzeczy mniej lub barziej legalnych</h3><p>Kali Linux - dystrybucja stworzona do Hackingu i Pentestingu<br/> Zawiera preinstalowane narzędzia do hakowania i pentestingu <br/> Zdecydowanie odradzany dla nowych użytkowników</p><br/><a href = "https://kali.org/get-kali/" class = "distro-button" id = "distro-button">Pobierz Kaliego</a>'
             $(sliderBackground).css( "background-color",  "rgb(6, 125, 204)");
-            $(sliderDownload).css( "background-color", "blue");   ;
         break;
 
         
